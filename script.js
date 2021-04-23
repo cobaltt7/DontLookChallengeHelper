ffb21fd539e64373924b7092010936b5ff9143f8
RedGuy12 <paul@reid-family.org>
2021-04-23 17:13:25 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("nav").innerHTML = `<ul class="navigation">
+<li class                                ="logo"><a href="https://scratch.mit.edu"></a></li>
+<li class                                ="link title"><a href="/">Don\'t Look! Helper</a></li>
+<li class                                ="link"><a href="/time">Start</a></li>
+<li class                                ="link"><a href="/counter">Battle</a></li>
+<li class                                ="link"><a href="/finish">Share</a></li>
+<li class                                ="link"><a href="/finish">Explore</a></li>
+</ul>`;
